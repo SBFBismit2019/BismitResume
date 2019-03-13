abbadd702592b1da03dc2e0f694534ca6fe64a07
ciferivalle <nataprawiraf@gmail.com>
2019-03-13 19:27:14 +0700

    initialize projecct

---
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<img class="img-responsive" src="images/bem.png" alt="" style="max-width:30px">
+<a class="ml-2 navbar-brand f-ptsans" href="#">Project Development</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link f-staat" href="#">Meet the Team <span class="sr-only">(current)</span></a>
+        </li>
+   </ul>
+
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+            {{-- <a class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a> --}}
+            <button class="bttn-stretch bttn-sm"> <span class="f-ptsans">Contact Us</span></button>
+        </li>
+    </ul>
+</div>
+</nav>
