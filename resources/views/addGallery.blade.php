72818b8b18e8a6653b70a9e84bffc883a76798ad
Muhammad Zuhdi <mzuhdi.zamrud@gmail.com>
2019-04-10 00:13:07 +0700

    add gallery

---
+++ resources/views/addGallery.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+</div>
+
+<div class="container text-center mt-5">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <img style="max-width:200px;" class="img-responsive" src="{{ asset('images/dum.png') }}" alt="">
+            <h3>Galler</h3>
+            <img class="img-responsive" style="max-width:60px;" src="{{asset('images/laravel.svg')}}" alt="">
+            <img class="img-responsive" style="max-width:30px;" src="{{asset('images/bt.svg')}}" alt="">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum illum omnis, quos tenetur rerum praesentium velit eum provident officiis facilis.</p>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/addgallery', function () {
+    return view('addGallery');
+});
